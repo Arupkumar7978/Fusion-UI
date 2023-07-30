18baa89b0521a060da254742061569cafd130a06
Arupkumar7978 <arupkumarpadhi68@gmail.com>
2023-07-30 18:06:50 +0530

    Removed Unwanted Css and added FusionTheme

---
+++ fusion-ui/src/Configurations/FusionPalettes.js
@@ -0,0 +1,18 @@
+const theme = 'LIGHT';
+
+const COLOR_PALETTE = {
+  LIGHT: {
+    BACKGROUND: '',
+    primary: '',
+    secondary: '',
+    tertiary: '',
+  },
+  DARK: {
+    BACKGROUND: '',
+    primary: '',
+    secondary: '',
+    tertiary: '',
+  },
+};
+
+export default COLOR_PALETTE[theme];
